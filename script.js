31a17c7669b509acc70e6fba54a0349684b2c564
Jason <121495300+jason21715@users.noreply.github.com>
2023-04-16 21:20:08 -0700

    Add chess gameboard function

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
-class chessBoard(){
-    constructor(){
-        
+class chessBoard {
+  showBoard() {
+    //row counter
+    for (let i = 0; i < 8; i++) {
+      let board = document.getElementById(`gameBoard`);
+      //column counter
+      for (let j = 0; j < 8; j++) {
+        let div = document.createElement("div");
+        let isBlack = false;
+        div.id = `${i}-${j}`;
+        div.textContent = "X";
+
+        (i + j) % 2 ? (isBlack = true) : (isBlack = false);
+        isBlack ? div.classList.add("black") : div.classList.add("white");
+        board.appendChild(div);
+        isBlack = !isBlack;
+      }
     }
+  }
+
+  gameBoard = [
+    [1, 2, 3, 4, 5, 6, 7, 8], //row 1
+    [1, 2, 3, 4, 5, 6, 7, 8], //row 2
+    [1, 2, 3, 4, 5, 6, 7, 8], //row 3
+    [1, 2, 3, 4, 5, 6, 7, 8], //row 4
+    [1, 2, 3, 4, 5, 6, 7, 8], //row 5
+    [1, 2, 3, 4, 5, 6, 7, 8], //row 6
+    [1, 2, 3, 4, 5, 6, 7, 8], //row 7
+    [1, 2, 3, 4, 5, 6, 7, 8], //row 8
+  ];
 }
+const board = new chessBoard();
+board.showBoard();
