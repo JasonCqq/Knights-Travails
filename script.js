338e30ddd494521c3617b5bf5814050fee6fcd4a
Jason <121495300+jason21715@users.noreply.github.com>
2023-04-19 04:10:02 -0700

    Modify createTree to pick the correct direction

--- script.js
+++ script.js
@@ -1,36 +1,3 @@
-class chessBoard {
-  //display chessBoard
-  showBoard() {
-    //row counter
-    for (let i = 0; i < 8; i++) {
-      let playBoard = document.getElementById("gameBoard");
-      //column counter
-      for (let j = 0; j < 8; j++) {
-        let div = document.createElement("div");
-        div.classList.add("piece");
-        let isBlack = false;
-        div.id = `${i}${j}`;
-
-        (i + j) % 2 ? (isBlack = true) : (isBlack = false);
-        isBlack ? div.classList.add("black") : div.classList.add("white");
-
-        playBoard.appendChild(div);
-        isBlack = !isBlack;
-      }
-    }
-  }
-  gameBoard = [
-    [1, 2, 3, 4, 5, 6, 7, 8], //row 1
-    [1, 2, 3, 4, 5, 6, 7, 8], //row 2
-    [1, 2, 3, 4, 5, 6, 7, 8], //row 3
-    [1, 2, 3, 4, 5, 6, 7, 8], //row 4
-    [1, 2, 3, 4, 5, 6, 7, 8], //row 5
-    [1, 2, 3, 4, 5, 6, 7, 8], //row 6
-    [1, 2, 3, 4, 5, 6, 7, 8], //row 7
-    [1, 2, 3, 4, 5, 6, 7, 8], //row 8
-  ];
-}
-
 class chessPiece {
   constructor(start, goal) {
     this.start = start;
@@ -44,26 +11,40 @@ class chessPiece {
     let i = currentPosition[0];
     let j = currentPosition[1];
 
-    // i = row, j = column
-    const possibleMoves = [
-      [i - 1, j + 2],
-      [i - 2, j + 1],
-      [i + 1, j + 2],
-      [i + 2, j + 1],
-      [i + 1, j - 2],
-      [i + 2, j - 1],
-      [i - 1, j - 2],
-      [i - 2, j - 2],
-    ];
-
-    //filter out all moves that are out of bounds.
-    function isOutOfBounds(num1, num2) {
-      if (num1 < 0 || num1 > 7 || num2 < 0 || num2 > 7) {
-        return false;
-      } else {
-        return [num1, num2];
-      }
+    let currentGoal = Array.from(this.goal).map(Number);
+    let side; //side = bottom-right / top-right etc
+    //finds the correct 1/4 portion
+    if (currentGoal[1] <= j) {
+      //left side
+      currentGoal[0] <= i ? (side = "top-left") : (side = "bottom-left");
+    } else if (currentGoal[1] > j) {
+      //right side
+      currentGoal[0] <= i ? (side = "top-right") : (side = "bottom-right");
     }
+
+    //finds the right moves to push into the right portion.
+    //i = row, j = column
+    let possibleMoves = [];
+    switch (side) {
+      case "top-left":
+        possibleMoves.push([i - 1, j - 2]);
+        possibleMoves.push([i - 2, j - 1]);
+        break;
+      case "bottom-left":
+        possibleMoves.push([i + 1, j - 2]);
+        possibleMoves.push([i + 2, j - 1]);
+        break;
+      case "top-right":
+        possibleMoves.push([i - 2, j + 1]);
+        possibleMoves.push([i - 1, j + 2]);
+        break;
+      case "bottom-right":
+        possibleMoves.push([i + 1, j + 2]);
+        possibleMoves.push([i + 2, j + 1]);
+        break;
+    }
+
+    //checks if it's out of bounds
     let filtered = possibleMoves.filter((array) =>
       isOutOfBounds(array[0], array[1])
     );
@@ -79,7 +60,44 @@ class chessPiece {
   }
 }
 
-//get start and end points function
+class move {
+  constructor(position, move1, move2) {
+    this.move1 = move1;
+    this.move2 = move2;
+    this.position = position;
+  }
+}
+
+//HELPER FUNCTIONS BELOW
+//filter out all moves that are out of bounds.
+function isOutOfBounds(num1, num2) {
+  if (num1 < 0 || num1 > 7 || num2 < 0 || num2 > 7) {
+    return false;
+  } else {
+    return [num1, num2];
+  }
+}
+//creates board, with each div's position id
+function showBoard() {
+  //row counter
+  for (let i = 0; i < 8; i++) {
+    let playBoard = document.getElementById("gameBoard");
+    //column counter
+    for (let j = 0; j < 8; j++) {
+      let div = document.createElement("div");
+      div.classList.add("piece");
+      let isBlack = false;
+      div.id = `${i}${j}`;
+
+      (i + j) % 2 ? (isBlack = true) : (isBlack = false);
+      isBlack ? div.classList.add("black") : div.classList.add("white");
+
+      playBoard.appendChild(div);
+      isBlack = !isBlack;
+    }
+  }
+}
+//get user input of start and goal helper function
 function placePieceAndEndPoint() {
   // piece/goal can only be placed once
   let piecePlaced = false;
@@ -112,9 +130,6 @@ function placePieceAndEndPoint() {
   });
 }
 
-const board = new chessBoard();
+showBoard();
 let knight = new chessPiece();
-board.showBoard();
 placePieceAndEndPoint();
-
-// console.log(chessPiece.log());
